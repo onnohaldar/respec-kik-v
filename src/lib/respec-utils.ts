c810f1fd1a526524e1ab150a4c906cb6e91bddc3
Amitava Haldar <onno.haldar@icloud.com>
2021-02-27 00:44:28 +0100

    refactor

--- src/lib/respec-utils.ts
+++ src/lib/respec-utils.ts
@@ -17,7 +17,24 @@ import { copySync } from 'cpx';
 export function parseMd2ResPec(resPecTemplatePath: string, mdContentPath: string, resPecOutputPath: string) {
     // init ResPec output with template to parse
     copySync(`${resPecTemplatePath}/**/*`, resPecOutputPath);
-    // read html template
+    
+    // parse md content
+    const settings = {
+        name: 'markdown-json',
+      	cwd: './',
+      	src: 'test/',
+        filePattern: '**/*.md',
+        ignore: "*(icon|input)*",
+        dist: 'output/output.json',
+        metadata: true,
+        server: false,
+        port: 3001
+    };
 
+    markdownJson(settings).then((data) => {
+        console.log('data:', data);
+    }).catch((err) => {
+        console.log('error:', err);
+    })
 
 }
