54dce9414020339f55f7c1e2c1ab77cfc56641d7
Amitava Haldar <onno.haldar@icloud.com>
2021-02-27 00:22:15 +0100

    refactor

--- src/bin/respecpremd-cli.ts
+++ src/bin/respecpremd-cli.ts
@@ -20,8 +20,10 @@ const colors = Colors;
 //
 // START CLI Script
 //
-let parms: string[] = [];
 
+if (!argv[4] || argv[5]) {
+    console.error('excact 3 parms needed: mdContentPath, resPecTemplatePath, resPecOutputPath');
+}
 
 
 console.log(`KIK-V - ${command} ${parms}`.bgRed.yellow.bold);
