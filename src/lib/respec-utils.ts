3f751291979854e52cc556f5302dec543b479025
Amitava Haldar <onno.haldar@icloud.com>
2021-02-27 11:21:08 +0100

    refactor

--- src/lib/respec-utils.ts
+++ src/lib/respec-utils.ts
@@ -5,7 +5,8 @@
 /**
  * Node Package Imports
  */
-//import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync, existsSync } from 'fs';
+import { join } from 'path';
 //import { default as marked } from 'marked';
 import { copySync } from 'cpx';
 
@@ -17,7 +18,23 @@ import { copySync } from 'cpx';
 export function parseMd2ResPec(resPecTemplatePath: string, mdContentPath: string, resPecOutputPath: string) {
     // init ResPec output with template to parse
     copySync(`${resPecTemplatePath}/**/*`, resPecOutputPath);
-    console.log(mdContentPath);
+    
+    // read template to parse
+    const outputHtmlIndexFilePath = join(resPecOutputPath, 'index.html');
+    let parsedHtmlIndexStr = readFileSync(outputHtmlIndexFilePath, 'utf-8');
+
+    // read content to parse
+    const abstractMdStr = readFileSync(join(mdContentPath, 'ABSCTRACT.md'), 'utf-8');
+    const summaryMdStr = readFileSync(join(mdContentPath, 'SUMMARY.md'), 'utf-8');
+    const conformanceMdStr = readFileSync(join(mdContentPath, 'CONFORMANCE.md'), 'utf-8');
+
+    // parse content
+    const parsedAbstractStr = abstractMdStr.split('/n');
+    console.log('parsedAbstractStr =');
+    console.log(parsedAbstractStr);
+
+    // write parsed content
+    writeFileSync(outputHtmlIndexFilePath, parsedHtmlIndexStr);
 
 
 }
