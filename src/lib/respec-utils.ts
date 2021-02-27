91632931d1dd9f5a9c5dd6a636c2ae58ad4a322a
Amitava Haldar <onno.haldar@icloud.com>
2021-02-27 12:01:16 +0100

    refactor

--- src/lib/respec-utils.ts
+++ src/lib/respec-utils.ts
@@ -16,6 +16,7 @@ import { copySync } from 'cpx';
 
 const ABSTRACT_MD = 'ABSTRACT.md'; 
 const SUMMARY_MD = 'SUMMARY.md';
+const SUMMARY_IDENT = '  ';
 const CONFORMANCE_MD = 'CONFORMANCE.md';
 
 export function parseMd2ResPec(resPecTemplatePath: string, mdContentPath: string, resPecOutputPath: string) {
@@ -28,14 +29,29 @@ export function parseMd2ResPec(resPecTemplatePath: string, mdContentPath: string
 
     // read content to parse
     const abstractMdStr = readFileSync(join(mdContentPath, ABSTRACT_MD), 'utf-8');
-    const summaryMdStr = readFileSync(join(mdContentPath, SUMMARY_MD), 'utf-8');
     const conformanceMdStr = readFileSync(join(mdContentPath, CONFORMANCE_MD), 'utf-8');
+    const summaryMdStr = readFileSync(join(mdContentPath, SUMMARY_MD), 'utf-8');
 
     // parse content
     // remove header from abstract
     const parsedAbstractStr = removeMdHeader(abstractMdStr);
-
     const parsedConformanceStr = removeMdHeader(conformanceMdStr);
+
+    const summaryLines = summaryMdStr.split('\n').filter(mdLine => mdLine.trimStart().startsWith('*'));
+
+    for (const summaryLine of summaryLines) {
+        console.log(summaryLine);
+        let htmlHeaderLevel = 2;
+        let summaryLineIndent = SUMMARY_IDENT;
+
+        while (summaryLine.startsWith(summaryLineIndent)) {
+            htmlHeaderLevel ++;
+            summaryLineIndent += SUMMARY_IDENT;
+        }
+
+        console.log('htmlHeaderLevel =', htmlHeaderLevel);
+
+    }
     
     // write parsed content
     writeFileSync(join(resPecOutputPath, ABSTRACT_MD), parsedAbstractStr);
--- test-input/content/SUMMARY.md
+++ test-input/content/SUMMARY.md
@@ -1,4 +1,4 @@
 # Summary
 
 * [Inleiding](README.md)
-  * []
+  * [Stap1](STAP1.md)
